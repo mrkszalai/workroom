c3f8118ba88a387a15d02be8fc68e053e30b7b53
Mark Szalai <szalai.mark@hotmail.com>
2016-11-30 00:28:12 +0100

    0.0.1

--- config/connections.js
+++ config/connections.js
@@ -60,8 +60,8 @@ module.exports.connections = {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
-    user: '********', //optional
-    password: '*********', //optional
+    user: '', //optional
+    password: '', //optional
     database: 'workroom' //optional
   },
 
